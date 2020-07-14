d8b115085914dc1df1e211495935a71625b5b890
Adam Hodges <ahodges@shipchain.io>
2020-07-14 11:02:05 -0400

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Dual Maps Autoload
+// @namespace    https://www.mapchannels.com/dualmaps7/map.htm
+// @version      0.1
+// @description  Automatically click 'View Map'
+// @author       Adam Hodges
+// @match        https://www.mapchannels.com/dualmaps7/*
+// @match        http://www.mapchannels.com/dualmaps7/*
+// @run-at       document-idle
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    viewDualMap();
+})();
